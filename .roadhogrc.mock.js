4eaf39a873013206ea1dd2965b77239545708cb6
wangtao <1049295955@qq.com>
2020-07-02 21:27:45 +0800

    分类第一天

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,41 @@
+export default
+{
+    'GET /api/cates': {
+        data:[
+            {    id:1,
+                name1:'热门品类',
+                url:'https://pic.midea.cn/h5/pic/202006/475454cc02b7.jpg?x-oss-process=image/format,webp/quality,Q_90&@100Q.jpg',
+                catelist:[
+                    {img:'https://pic.midea.cn/h5/img/mall/category_sub/category_10002.png?x-oss-process=image/format,webp/quality,Q_90&t=20191219'},
+                 
+                 
+                ],
+                // name2:'热门单品',
+                // shopl:{img:'xxx.jpg'},
+                // nameist:[
+                //     {img:'xxx.jpg'}
+                // ],
+            },
+            {    id:2,
+                name1:'热门品类',
+                url:'https://pic.midea.cn/h5/pic/202006/2abddeb7ae3f.jpg?x-oss-process=image/format,webp/quality,Q_90&@100Q.jpg',
+                catelist:[
+                    {img:'https://pic.midea.cn/h5/img/mall/category_sub/category_10002.png?x-oss-process=image/format,webp/quality,Q_90&t=20191219'},
+                 
+                 
+                ],
+                // name2:'热门单品',
+                // shopl:{img:'xxx.jpg'},
+                // nameist:[
+                //     {img:'xxx.jpg'}
+                // ],
+            },
+          
+            
+        ]
+           
+          
+       
+    },
+}
 
-export default {
-};
